a6415314db3d9b5b76504cac1adcf77da7e0c366
Rupesh Jha <rupeshjha7796@gmail.com>
2022-12-17 12:35:19 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,84 @@
+
+
+function first(){
+  document.getElementById("text").style.fontFamily="font1"
+}
+
+document.getElementById("font2").addEventListener("click",function2);
+    function function2(){
+        document.getElementById("text").style.fontFamily="font2";
+    }  
+
+    document.getElementById("font3").addEventListener("click",function3);
+    function function3(){
+        document.getElementById("text").style.fontFamily="font3";
+    }  
+
+    document.getElementById("font4").addEventListener("click",function4);
+    function function4(){
+        document.getElementById("text").style.fontFamily="font4";
+    } 
+
+    document.getElementById("font5").addEventListener("click",function5);
+    function function5(){
+        document.getElementById("text").style.fontFamily="font5";
+    } 
+
+    document.getElementById("font6").addEventListener("click",function6);
+    function function6(){
+        document.getElementById("text").style.fontFamily="font6";
+    } 
+
+    document.getElementById("font7").addEventListener("click",function7);
+    function function7(){
+        document.getElementById("text").style.fontFamily="font7";
+    } 
+
+    document.getElementById("font8").addEventListener("click",function8);
+    function function8(){
+        document.getElementById("text").style.fontFamily="font8";
+    } 
+
+    document.getElementById("font9").addEventListener("click",function9);
+    function function9(){
+        document.getElementById("text").style.fontFamily="font9";
+    } 
+
+    document.getElementById("font10").addEventListener("click",function10);
+    function function10(){
+        document.getElementById("text").style.fontFamily="font10";
+    } 
+
+    document.getElementById("font11").addEventListener("click",function11);
+    function function11(){
+        document.getElementById("text").style.fontFamily="font11";
+    } 
+
+    document.getElementById("font12").addEventListener("click",function12);
+    function function12(){
+        document.getElementById("text").style.fontFamily="font12";
+    } 
+
+    document.getElementById("color-box1").addEventListener("click",function13);
+    function function13(){
+        document.getElementById("text").style.color="red";
+        document.getElementById("text").style.textShadow="0 0 10px red,0 0 50px red,0 0 21px #fff";
+    } 
+
+    document.getElementById("color-box2").addEventListener("click",function14);
+    function function14(){
+        document.getElementById("text").style.color="pink";
+        document.getElementById("text").style.textShadow="0 0 10px pink,0 0 50px pink,0 0 21px red";
+    } 
+
+    document.getElementById("color-box3").addEventListener("click",function15);
+    function function15(){
+        document.getElementById("text").style.color="blue";
+        document.getElementById("text").style.textShadow="0 0 10px blue,0 0 50px blue,0 0 21px #fff";
+    } 
+
+    document.getElementById("color-box4").addEventListener("click",function16);
+    function function16(){
+        document.getElementById("text").style.color="yellow";
+        document.getElementById("text").style.textShadow="0 0 10px red,0 0 50px red,0 0 21px red";
+    } 
